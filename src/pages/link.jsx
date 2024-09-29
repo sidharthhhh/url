275c22757f2059600023945a91f620f694602832
sidharthhhh <swayam1044@gmail.com>
2024-09-29 19:21:44 +0530

    save

--- src/pages/link.jsx
+++ src/pages/link.jsx
@@ -76,11 +76,11 @@ const LinkPage = () => {
             {url?.title}
           </span>
           <a
-            href={`https://trimrr.in/${link}`}
+            href={`https://url-zeta-eight.vercel.app/${link}`}
             target="_blank"
             className="text-3xl sm:text-4xl text-blue-400 font-bold hover:underline cursor-pointer"
           >
-            https://trimrr.in/{link}
+            https://url-zeta-eight.vercel.app/{link}
           </a>
           <a
             href={url?.original_url}
@@ -97,7 +97,7 @@ const LinkPage = () => {
             <Button
               variant="ghost"
               onClick={() =>
-                navigator.clipboard.writeText(`https://trimrr.in/${link}`)
+                navigator.clipboard.writeText(`https://url-zeta-eight.vercel.app/${link}`)
               }
             >
               <Copy />
